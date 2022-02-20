d2253f2c12c73e41f512954cb37d14c395c8eee9
Pulkit Verma <pulkitverma25@gmail.com>
2022-02-20 12:17:31 -0700

    plot script done

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,33 +1,98 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
 
-# Load data
+
+# Load data_t3
 data = pd.read_csv('hw1_results.csv')
 
-# only keep the data[' Time'] column, data['Dimension'] column, data['Algorithm'] column
+# only keep the data_t3[' Nodes Expanded'] column, data_t3['Dimension'] column, data_t3['Algorithm'] column
 
-data2 = data[['Dimension', ' Algorithm', ' Time']]
+data = data[['Dimension', ' Algorithm', ' Nodes Expanded', ' Time']]
 
-# #calculate mean of data[' Time'] column for same data['algorithm'] and data['dimension']
+# #calculate mean of data_t3[' Nodes Expanded'] column for same data_t3['algorithm'] and data_t3['dimension']
 
-data3 = data2.groupby(['Dimension', ' Algorithm'])[' Time'].mean()
+data_t2 = data.groupby(['Dimension', ' Algorithm'])[' Time'].mean()
+data_t3= data.groupby(['Dimension', ' Algorithm'])[' Nodes Expanded'].mean()
 
-
-# put all astar data into a list
-
-astar_list = []
-for i in range(1, len(data3)):
-    if data3.index[i][1] == 'astar':
-        astar_list.append(data3.index[i][0])
+data_t2 = data_t2.reset_index()
+data_t3 = data_t3.reset_index()
 
 
-print(astar_list)
+# put all algorithms data_t3 into a seperate lists
+bfs_list_t2 = []
+ucs_list_t2 = []
+gbfs_list_t2 = []
+astar_list_t2 = []
+custom_astar_list_t2 = []
 
-    
+bfs_list_t3 = []
+ucs_list_t3 = []
+gbfs_list_t3 = []
+astar_list_t3 = []
+custom_astar_list_t3 = []
 
 
-# data2.plot(x=' Algorithm', y='Time', kind='line')
-# data2.plot(x=' Dimension', y='Time', kind='line')
-plt.legend(loc='upper right')
-#plt.show()
+
+
+for i in range(len(data_t2)):
+    if data_t2.iloc[i][' Algorithm'] == ' bfs':
+        bfs_list_t2.append((data_t2.iloc[i]['Dimension'], data_t2.iloc[i][' Time']))
+    if data_t2.iloc[i][' Algorithm'] == ' ucs':
+        ucs_list_t2.append((data_t2.iloc[i]['Dimension'], data_t2.iloc[i][' Time']))
+    if data_t2.iloc[i][' Algorithm'] == ' gbfs':
+        gbfs_list_t2.append((data_t2.iloc[i]['Dimension'], data_t2.iloc[i][' Time']))
+    if data_t2.iloc[i][' Algorithm'] == ' astar':
+        astar_list_t2.append((data_t2.iloc[i]['Dimension'], data_t2.iloc[i][' Time']))
+    if data_t2.iloc[i][' Algorithm'] == ' custom-astar':
+        custom_astar_list_t2.append((data_t2.iloc[i]['Dimension'], data_t2.iloc[i][' Time']))
+
+
+for i in range(len(data_t3)):
+    if data_t3.iloc[i][' Algorithm'] == ' bfs':
+        bfs_list_t3.append((data_t3.iloc[i]['Dimension'], data_t3.iloc[i][' Nodes Expanded']))
+    if data_t3.iloc[i][' Algorithm'] == ' ucs':
+        ucs_list_t3.append((data_t3.iloc[i]['Dimension'], data_t3.iloc[i][' Nodes Expanded']))
+    if data_t3.iloc[i][' Algorithm'] == ' gbfs':
+        gbfs_list_t3.append((data_t3.iloc[i]['Dimension'], data_t3.iloc[i][' Nodes Expanded']))
+    if data_t3.iloc[i][' Algorithm'] == ' astar':
+        astar_list_t3.append((data_t3.iloc[i]['Dimension'], data_t3.iloc[i][' Nodes Expanded']))
+    if data_t3.iloc[i][' Algorithm'] == ' custom-astar':
+        custom_astar_list_t3.append((data_t3.iloc[i]['Dimension'], data_t3.iloc[i][' Nodes Expanded']))
+
+
+
+
+#plot two line plots
+
+#plot t2
+# time vs dimesntion for all algorithms
+plot1 = plt.figure(1)
+
+plt.plot([i[0] for i in bfs_list_t2], [i[1] for i in bfs_list_t2], label='bfs')
+plt.plot([i[0] for i in ucs_list_t2], [i[1] for i in ucs_list_t2], label='ucs')
+plt.plot([i[0] for i in gbfs_list_t2], [i[1] for i in gbfs_list_t2], label='gbfs')
+plt.plot([i[0] for i in astar_list_t2], [i[1] for i in astar_list_t2], label='astar')
+plt.plot([i[0] for i in custom_astar_list_t2], [i[1] for i in custom_astar_list_t2], label='custom-astar')
+plt.legend()
+plt.xlabel('Dimension')
+plt.ylabel('Time')
+plt.title('Time vs Dimension')
+
+# plot t3
+# nodes vs dimesntion for all algorithms
+
+plot2 = plt.figure(2)
+plt.plot([i[0] for i in bfs_list_t3], [i[1] for i in bfs_list_t3], label='bfs')
+plt.plot([i[0] for i in ucs_list_t3], [i[1] for i in ucs_list_t3], label='ucs')
+plt.plot([i[0] for i in gbfs_list_t3], [i[1] for i in gbfs_list_t3], label='gbfs')
+plt.plot([i[0] for i in astar_list_t3], [i[1] for i in astar_list_t3], label='astar')
+plt.plot([i[0] for i in custom_astar_list_t3], [i[1] for i in custom_astar_list_t3], label='custom-astar')
+plt.legend()
+plt.xlabel('Dimension')
+plt.ylabel('Nodes Expanded')
+plt.title('Nodes Expanded vs Dimension')
+
+
+plt.show()
+
+
